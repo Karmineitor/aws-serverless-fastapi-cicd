9f84609b9dabc171ca5140ae1222913a468af71a
Roelver Duarte <dejeentrar@hotmail.com>
2021-11-28 12:50:58 -0600

    Versión inicial
    Se configuran templates de página principal y de macros

--- Main.py
+++ Main.py
@@ -3,16 +3,24 @@ from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import logging
 app = FastAPI()
 
 #Damos de alta cosas de Jinja
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+#Definimos el log
+logger = logging.getLogger("foo")
+
 class Item(BaseModel):
     name: str
     price: float
     is_offer: bool = None
+class producto(BaseModel):
+    titulo: str
+    texto: str
+    precio: int
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -25,3 +33,19 @@ def create_item(item_id: int, item: Item):
 @app.get("/pag/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
+@app.get("/perfil/{cantidad}", response_class=HTMLResponse)
+async def read_item(request: Request, cantidad: int):
+    return templates.TemplateResponse("hijo.html", {"request": request
+                                                    , "cantidad": cantidad
+                                                    , "nombre": "Roelver"
+                                                    , "subtitulo": "Hola"
+                                                    , "texto": "TextoRoelver"
+                                                    , "lista": ["apple", "banana", "cherry"]
+                                                    , "precio": 777})
+@app.put("/ropa/{cantidad}")
+async def FuncionTemplateMacro(request: Request, productos: producto):
+    for producto in productos:
+        logger.info("hello")
+    return templates.TemplateResponse("templateMacro.html", {"request": request
+                                                            , "productos" : productos
+                                                            , "nombre": "Roelver"})
