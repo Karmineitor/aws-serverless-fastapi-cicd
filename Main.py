3c6ed769bc0e220125b86eda7fadcd494e6addaa
Roelver Duarte <dejeentrar@hotmail.com>
2021-11-28 00:52:17 -0600

    Primer acercamiento Web
    Después de probar las capacidades de backend se inicia el trabajo con jinja2, el cuál funciona al primer intento tomando variables y código CSS

--- Main.py
+++ Main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
+
+#Damos de alta cosas de Jinja
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+@app.put("/items/{item_id}")
+def create_item(item_id: int, item: Item):
+    return {"item_price": item.price, "item_id": item_id}
+@app.get("/pag/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
